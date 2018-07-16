5868cb1c73633cf02f69588ad16ab76ca792cc1b
MEA\Aviral.Yadav <aviral.yadav@in.ey.com>
2018-07-16 16:23:25 +0530

    Flow validation added

--- react-flow-type-checking/src/App.js
+++ react-flow-type-checking/src/App.js
@@ -1,6 +1,7 @@
 //@flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
+// import { } from 'react-dom';
 import './App.css';
 
 type TestProps = {
@@ -16,19 +17,37 @@ type testFuncType = {
   name: string
 }
 
-const testFunc = (props: testFuncType) => {
-return (<div>{props.name}</div>);
+function TestFunc(props: testFuncType) {
+
+  return <div>{props.name}</div>;
 }
+// TestFunc.defaultProps = {   ////default props for stateless component 
+//   name: 'abhi'
+// }
 
 class Test extends Component<TestProps, TestState> {  //  first param is props type and secpnd is state type <propsType, stateType>
   state = {
     message: 'hello'
   }
-  static defaultProps = {
-    str: 'abc'
+  // static defaultProps = {
+  //   str: 'abc'
+  // }
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps', nextProps, this.props);
   }
-  
-  render (){
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate - test');
+    return true;
+  }
+  componentWillUpdate(){
+    console.log('componentWillUpdate - test');
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate - test');
+  }
+
+  render() {
+    console.log('render - test')
     return (
       <div>
         <h3>{this.props.str}</h3>
@@ -40,16 +59,48 @@ class Test extends Component<TestProps, TestState> {  //  first param is props t
 }
 
 class App extends Component<{}> {
+  constructor(props){
+    super(props);
+    console.log('constructor');
+  }
+  state = {
+    content: ''
+  }
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps', nextProps);
+  }
+  setNext = () => {
+    this.setState({content: 'Hello content'});
+  }
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+  componentWillUpdate(){
+    console.log('componentWillUpdate');
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate');
+  }
   render() {
+    console.log('render');
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {/* <Test str={'test'} /> */}
-        <Test /> {/* for default prop value */}
-        <testFunc name={'avi'} />
+        <Test str={'test'} />
+        {/* <Test /> for default prop value */}
+        <TestFunc name={'abhi'} />
+        <p>{this.state.content}</p>
+        <button onClick={this.setNext} >Click</button>
       </div>
     );
   }
